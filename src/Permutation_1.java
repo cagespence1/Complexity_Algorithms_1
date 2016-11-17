ae7e27a464e241ab77333ed5e03200663057ed19
Cage Spence <cage.spence@gmail.com>
2016-11-17 14:20:30 +0100

    permutations_1 assignment complete

--- src/Permutation_1.java
+++ src/Permutation_1.java
@@ -10,10 +10,10 @@ public class Permutation_1 {
 
     public void run() {
 
-        System.out.print("input number of permutations: ");
-
         //initialize the scanner
         Scanner inputScanner = new Scanner(System.in);
+
+        System.out.print("input number of permutations: ");
 
         //get scanner input
         int n = inputScanner.nextInt();
@@ -24,10 +24,16 @@ public class Permutation_1 {
         //create the arary with length n
         int[] permutations = new int[n];
 
+        for(int i=0; i<permutations.length; i++){
+            permutations[i] = -1;
+        }
+
         boolean same = true;
 
         //main for-loop
         for (int i = 0; i < n; i++) {
+
+            same = true;
             while (same) {
 
                 int x = randInt(j);
@@ -35,19 +41,25 @@ public class Permutation_1 {
                 for (int k = 0; k < n; k++) {
 
                     if (x == permutations[k]){
-                        k = n+1;
+                        break;
                     }
 
-                    if (k == n){
+                    if (k == n-1){
                         same = false;
                         permutations[i] = x;
                     }
                 }
             }
+
+            if (i == n-1){
+
+            }
         }
 
-        System.out.println(permutations);
-        
+        for (int i = 0; i < n; i++) {
+            System.out.println(permutations[i]);
+        }
+
     }
 
     public int randInt(int j){
